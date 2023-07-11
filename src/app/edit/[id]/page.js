89667cf7742aef60bd0d9d43742943ea6f8fd970
brian <liar88828@gmail.com>
2023-07-11 12:43:47 +0700

    update

---
+++ src/app/edit/[id]/page.js
@@ -0,0 +1,70 @@
+import dbConnect from '@/app/utils/dbConnection'
+import Todo from '../../../../lib/model/Todo'
+import { Suspense } from 'react'
+import Loading from '../../loading'
+import { redirect } from 'next/navigation'
+
+export default async function edit({ params }) {
+	dbConnect()
+	const todos = await Todo.findOne({ _id: params.id })
+
+	async function updateTodo(data) {
+		'use server'
+		let title = await data.get('title')?.valueOf()
+		let todo = await data.get('todo')?.valueOf()
+		let updateTodo = await Todo.findByIdAndUpdate(
+			{ _id: params.id },
+			{ title, todo },
+		)
+		console.log(updateTodo)
+		redirect('/show')
+	}
+
+	return (
+		<main className='m-10 space-y-5'>
+			<h1 className='text-xl font-bold'>Edit Todo</h1>
+
+			<Suspense fallback={<Loading />}>
+				<form
+					action={updateTodo}
+					className='flex flex-col gap-2'>
+					<div>
+						<label
+							htmlFor='title'
+							className='text-lg'>
+							Title
+						</label>
+						<input
+							type='text'
+							name='title'
+							id='title'
+							className='w-[100%] bg-slate-200 h-10 p-3'
+							defaultValue={todos.title}
+						/>
+					</div>
+
+					<label
+						htmlFor='todo'
+						className='text-lg'>
+						Todo
+						<input
+							type='text'
+							name='todo'
+							id='todo'
+							className='w-[100%] bg-slate-200 h-10 p-3'
+							defaultValue={todos.todo}
+						/>
+					</label>
+
+					<div className=' flex justify-center mt-5'>
+						<button
+							type='submit'
+							className=' w-1/2 p-3 bg-yellow-400 bold hover:bg-orange-500 hover:text-white'>
+							Submit
+						</button>
+					</div>
+				</form>
+			</Suspense>
+		</main>
+	)
+}
--- src/app/show/page.js
+++ src/app/show/page.js
@@ -1,46 +1,52 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import dbConnect from '../utils/dbConnection'
 import Todo from '../../../lib/model/Todo'
+import Link from 'next/link'
+import Loading from '../loading'
 
 export default async function Show ()
 {
-  dbConnect()
-  const todos = await Todo.find()
-  
-  return (
-    <div>
-      <main className="m-10 space-y-5">
-        <h1 className="text-xl font-bold">Todos</h1>
-        <div className="">
-          <ul className="flex flex-bold">
-            <li className='flex-1'>No</li>
-            <li className='flex-1'>Title</li>
-            <li className='flex-1'>Todo</li>
-            <li className='flex-1'>options</li>
-          </ul>
-          <hr />
-          { todos.map( (t,i) => (
-            <>
-              <ul className="flex" key={t._id}>
-                <li className='flex-1'>{i+1} </li>    
-                <li className='flex-1'>{ t.title}</li>
-                <li className='flex-1'>{ t.todo}</li>
-                <li className='flex-1'>
-                  <div className="flex">
-                    <button className='p-2 m-2 bg-red-600 text-white hover:cursor-pointer'> Delete
-                    </button>
-                    <button className='p-2 m-2 bg-blue-600 text-white hover:cursor-pointer'> Edit
-                    </button>
+	await dbConnect()
+	const todos = await Todo.find()
 
-                  </div>
-                </li>
-              </ul>
-            </>
-          ))}
-        </div>
-      </main>  
-
-
-    </div>
-  )
+	return (
+		<>
+			<main className='m-10 space-y-5'>
+				<h1 className='text-xl font-bold'>Todos</h1>
+				<div>
+					<ul className='flex flex-bold'>
+						<li className='flex-1'>No.</li>
+						<li className='flex-1'>Title</li>
+						<li className='flex-1'>Todo</li>
+						<li className='flex-1'>options</li>
+					</ul>
+					<hr />
+					<Suspense fallback={ <Loading /> }>
+						{ ( todos.map( ( t, i ) => (
+							<>
+								<ul className='flex' key={ t._id }>
+									<li className='flex-1'>{ i + 1 } </li>
+									<li className='flex-1'>{ t.title }</li>
+									<li className='flex-1'>{ t.todo }</li>
+									<li className='flex-1'>
+										<div className='flex'>
+											<button className='p-2 m-2 bg-red-600 text-white hover:cursor-pointer'>
+												Delete
+											</button>
+											<Link href={ `/edit/${ t._id }` }>
+												<button className='p-2 m-2 bg-blue-600 text-white hover:cursor-pointer'>
+													Edit
+												</button>
+											</Link>
+										</div>
+									</li>
+								</ul>
+							</>
+						) )
+						) }
+					</Suspense>
+				</div>
+			</main>
+		</>
+	)
 }
